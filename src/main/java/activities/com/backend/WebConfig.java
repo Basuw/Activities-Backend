fbdd56f4c353c2731e2ae9ade07e5510ba15ed78
bajacqueli <bastien.jacquelin@etu.uca.fr>
2024-01-07 11:43:28 +0100

    add webconfig to enable local connection

--- src/main/java/activities/com/backend/WebConfig.java
+++ src/main/java/activities/com/backend/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/activities/reminder/user/**")
+        registry.addMapping("/activities/**")
                 .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
