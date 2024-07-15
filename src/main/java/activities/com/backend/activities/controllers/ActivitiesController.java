3344ca31ce33359b9e514ae178e77a11da21620e
bajacqueli <bastien.jacquelin@etu.uca.fr>
2024-07-15 12:05:18 +0200

    Add a proper service to Activity

--- src/main/java/activities/com/backend/activities/controllers/ActivitiesController.java
+++ src/main/java/activities/com/backend/activities/controllers/ActivitiesController.java
@@ -1,7 +1,7 @@
 package activities.com.backend.activities.controllers;
 
 import activities.com.backend.activities.models.Activity;
-import activities.com.backend.activities.repositories.ActivityRepository;
+import activities.com.backend.activities.services.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,17 +13,17 @@ import java.util.List;
 @RequestMapping(ACTIVITY)
 public class ActivitiesController {
 
-    private final ActivityRepository activityRepository;
+    private final ActivityService activityService;
 
     @Autowired
-    public ActivitiesController(ActivityRepository activityRepository){
-        this.activityRepository = activityRepository;
+    public ActivitiesController(ActivityService activityService){
+        this.activityService = activityService;
     }
 
     @GetMapping("/activity")
-    public List<Activity> getAllActivities(){
+    public ResponseEntity<List<Activity>> getAllActivities(){
         try {
-            return this.activityRepository.findAll();
+            return ResponseEntity.ok().body(activityService.getAllActivities());
         }catch (RuntimeException exception){
             throw new RuntimeException("Error getting all activities");
         }
@@ -32,7 +32,7 @@ public class ActivitiesController {
     @GetMapping("/activity/{id}")
     public ResponseEntity<Activity> getActivitiesById(@PathVariable long id){
         try {
-            return ResponseEntity.ok().body(this.activityRepository.findById(id));
+            return ResponseEntity.ok().body(activityService.getActivitiesById(id));
         }catch (RuntimeException exception){
             throw new RuntimeException("Error getting all activities");
         }
@@ -40,16 +40,15 @@ public class ActivitiesController {
     @PostMapping("/activity")
     public ResponseEntity<Activity> addActivity(@RequestBody Activity activity){
         try {
-            this.activityRepository.save(activity);
+            return ResponseEntity.ok().body(activityService.addActivity(activity));
         }catch (RuntimeException exception){
             throw new RuntimeException("Error adding activity");
         }
-        return ResponseEntity.ok().body(activity);
     }
     @DeleteMapping("/activity/{id}")
     public ResponseEntity<String> deleteActivity(@PathVariable long id){
         try {
-            this.activityRepository.deleteById(id);
+            activityService.deleteActivity(id);
         }catch (RuntimeException exception){
             throw new RuntimeException("Error adding activity");
         }
---
+++ src/main/java/activities/com/backend/activities/services/ActivityService.java
@@ -0,0 +1,48 @@
+package activities.com.backend.activities.services;
+
+import activities.com.backend.activities.models.Activity;
+import activities.com.backend.activities.repositories.ActivityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ActivityService {
+    private final ActivityRepository activityRepository;
+
+    public ActivityService(ActivityRepository activityRepository){
+        this.activityRepository = activityRepository;
+    }
+
+    public List<Activity> getAllActivities(){
+        try {
+            return this.activityRepository.findAll();
+        }catch (RuntimeException exception){
+            throw new RuntimeException("Error getting all activities");
+        }
+    }
+
+    public Activity getActivitiesById(long id){
+        try {
+            return activityRepository.findById(id);
+        }catch (RuntimeException exception){
+            throw new RuntimeException("Error getting all activities");
+        }
+    }
+
+    public Activity addActivity(Activity activity){
+        try {
+            return activityRepository.save(activity);
+        }catch (RuntimeException exception){
+            throw new RuntimeException("Error adding activity");
+        }
+    }
+
+    public void deleteActivity(long id){
+        try {
+            this.activityRepository.deleteById(id);
+        }catch (RuntimeException exception){
+            throw new RuntimeException("Error adding activity");
+        }
+    }
+}
