9f1e7d5c640f3e5ae24297380576bab0c8841128
bajacqueli <bastien.jacquelin@etu.uca.fr>
2024-07-23 11:57:58 +0200

    [FIX] code smells 💩

--- src/main/java/activities/com/backend/activities/dto/ActivityDTO.java
+++ src/main/java/activities/com/backend/activities/dto/ActivityDTO.java
@@ -1,17 +1,7 @@
 package activities.com.backend.activities.dto;
 
-import activities.com.backend.activities.models.User;
-import jakarta.annotation.Nullable;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.Date;
-
-import static jakarta.persistence.GenerationType.SEQUENCE;
 
 @Data
 @AllArgsConstructor
